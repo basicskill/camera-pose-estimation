4f6e5a7dddae0a79f3365fa3e06db7bfffd72218
mladen <mladenbasic99@gmail.com>
2020-08-07 19:11:01 +0200

    training cosmetics

--- model.py
+++ model.py
@@ -38,6 +38,7 @@ class MyNet(nn.Module):
         spp = torch.cat((spp1, spp2), dim=1)
 
         t = self.fcT(spp)
+        # TODO: Modifikovati za Ojlerove uglove
         q = self.fcQ(spp)
 
         return t, q
